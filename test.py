08b187aa1d5b9d4c0b7bb0e13b7ce8f0e024a942
Daniel-02 <daniel_toledo02@hotmail.com>
2018-06-30 15:33:38 -0300

    Ordem certa

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import nltk
 import pickle
-from nltk.corpus import wordnet as wn
+from nltk.stem import WordNetLemmatizer
 
 text = "Falta muito pouco pra excelente. Sugiro melhorar a parte de notícias e incluir informações sobre o BID, por exemplo."
 tagger = pickle.load(open("tagger.pkl", 'rb'))
@@ -10,12 +10,14 @@ stemmer = nltk.stem.RSLPStemmer()
 sentences = portuguese_sent_tokenizer.tokenize(text)
 tokens = [nltk.word_tokenize(sentence) for sentence in sentences]
 
-stemmed_sentences = []
-for token_s in tokens:
-    stemmed_tokens = []
-    for token in token_s:
-        stemmed_tokens.append(stemmer.stem(token))
-    stemmed_sentences.append(stemmed_tokens)
-
-tags = [tagger.tag(token) for token in stemmed_sentences]
+tags = [tagger.tag(token) for token in tokens]
 print(tags)
+
+stemmed_sentences = []
+for tags_s in tags:
+    stemmed_tokens = []
+    for token in tags_s:
+        stemmed_tokens.append([stemmer.stem(token[0]), token[1]])
+        print(stemmed_tokens)
+    stemmed_sentences.append(stemmed_tokens)
+print(stemmed_sentences)
