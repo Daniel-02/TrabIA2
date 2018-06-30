436066a5f98479c33fdd62856bc6a8c1ba4f78a1
Daniel-02 <daniel_toledo02@hotmail.com>
2018-06-30 14:45:06 -0300

    Lematização

--- test.py
+++ test.py
@@ -1,11 +1,21 @@
 import nltk
 import pickle
+from nltk.corpus import wordnet as wn
 
-text = "Socorro! Cai da escada."
+text = "Falta muito pouco pra excelente. Sugiro melhorar a parte de notícias e incluir informações sobre o BID, por exemplo."
 tagger = pickle.load(open("tagger.pkl", 'rb'))
 portuguese_sent_tokenizer = nltk.data.load("tokenizers/punkt/portuguese.pickle")
+stemmer = nltk.stem.RSLPStemmer()
+
 sentences = portuguese_sent_tokenizer.tokenize(text)
+tokens = [nltk.word_tokenize(sentence) for sentence in sentences]
 
-tags = [tagger.tag(nltk.word_tokenize(sentence)) for sentence in sentences]
+stemmed_sentences = []
+for token_s in tokens:
+    stemmed_tokens = []
+    for token in token_s:
+        stemmed_tokens.append(stemmer.stem(token))
+    stemmed_sentences.append(stemmed_tokens)
 
+tags = [tagger.tag(token) for token in stemmed_sentences]
 print(tags)
