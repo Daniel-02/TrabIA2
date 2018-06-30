d7260b8db323e686ebefdaaaaebbb5727862cffd
Daniel-02 <daniel_toledo02@hotmail.com>
2018-06-30 15:39:55 -0300

    Comentários

--- test.py
+++ test.py
@@ -3,21 +3,24 @@ import pickle
 from nltk.stem import WordNetLemmatizer
 
 text = "Falta muito pouco pra excelente. Sugiro melhorar a parte de notícias e incluir informações sobre o BID, por exemplo."
-tagger = pickle.load(open("tagger.pkl", 'rb'))
-portuguese_sent_tokenizer = nltk.data.load("tokenizers/punkt/portuguese.pickle")
-stemmer = nltk.stem.RSLPStemmer()
 
+# Tokenização das sentenças
+portuguese_sent_tokenizer = nltk.data.load("tokenizers/punkt/portuguese.pickle")
 sentences = portuguese_sent_tokenizer.tokenize(text)
 tokens = [nltk.word_tokenize(sentence) for sentence in sentences]
 
+# POS tagging
+tagger = pickle.load(open("tagger.pkl", 'rb'))
 tags = [tagger.tag(token) for token in tokens]
 print(tags)
 
+
+# Stemming usando RSLPStemmer
+stemmer = nltk.stem.RSLPStemmer()
 stemmed_sentences = []
 for tags_s in tags:
     stemmed_tokens = []
     for token in tags_s:
         stemmed_tokens.append([stemmer.stem(token[0]), token[1]])
-        print(stemmed_tokens)
     stemmed_sentences.append(stemmed_tokens)
 print(stemmed_sentences)
